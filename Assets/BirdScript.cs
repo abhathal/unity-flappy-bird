039421f0fc4ac948872abe50606219c3b0e0d513
Arvin Bhathal <arvinder.bhathal@gmail.com>
2024-12-30 19:51:41 -0500

    Add upward velocity

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.name = "Bob Birdington";   
+        gameObject.name = "Bob Birdington";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRigidbody.linearVelocity = Vector2.up * 10;
     }
 }
